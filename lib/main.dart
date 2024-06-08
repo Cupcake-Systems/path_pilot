6a0994af42936256e178ee641b02dff3f54d78a2
Finn Drünert <finndruenert@gmail.com>
2024-06-08 20:24:38 +0200

    Remove AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Robi Line Drawer',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan, brightness: Brightness.dark),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.cyan, brightness: Brightness.dark),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -31,12 +32,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Robi Line Drawer"),
-      ),
-      body: const Editor(),
-    );
+    return const Scaffold(body: Editor());
   }
 }
