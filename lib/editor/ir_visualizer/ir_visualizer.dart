6178fca42f6114cb4c149ac4e5dcde5f89b73970
Finn Drünert <finndruenert@gmail.com>
2024-11-17 22:04:41 +0100

    bf

--- lib/editor/ir_visualizer/ir_visualizer.dart
+++ lib/editor/ir_visualizer/ir_visualizer.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_resizable_container/flutter_resizable_container.dart';
 import 'package:path_pilot/editor/painters/ir_read_painter.dart';
 import 'package:path_pilot/file_browser.dart';
-import 'package:vector_math/vector_math.dart' show Vector2;
 
 import '../../robi_api/ir_read_api.dart';
 import '../../robi_api/robi_utils.dart';
@@ -10,7 +9,7 @@ import '../interactable_visualizer.dart';
 import 'approximation_settings_widget.dart';
 import 'ir_reading_info.dart';
 
-class IrVisualizerWidget extends StatefulWidget {
+class IrVisualizerWidget extends StatelessWidget {
   final IrReadResult irReadResult;
   final RobiConfig robiConfig;
   final double time;
@@ -27,71 +26,64 @@ class IrVisualizerWidget extends StatefulWidget {
   });
 
   @override
-  State<IrVisualizerWidget> createState() => _IrVisualizerWidgetState();
-}
-
-class _IrVisualizerWidgetState extends State<IrVisualizerWidget> {
-  late final IrCalculatorResult irCalculatorResult = IrCalculator.calculate(widget.irReadResult, widget.robiConfig);
-
-  IrReadPainterSettings irReadPainterSettings = defaultIrReadPainterSettings;
-
-  List<Vector2>? irPathApproximation;
-
-  @override
   Widget build(BuildContext context) {
-    irPathApproximation = IrCalculator.pathApproximation(
-      irCalculatorResult,
-      irReadPainterSettings.irInclusionThreshold,
-      irReadPainterSettings.ramerDouglasPeuckerTolerance,
-    );
+    final IrCalculatorResult irCalculatorResult = IrCalculator.calculate(irReadResult, robiConfig);
+    IrReadPainterSettings irReadPainterSettings = defaultIrReadPainterSettings;
 
-    InteractableIrVisualizer? visualizer;
-    Widget? editor;
+    return StatefulBuilder(builder: (context, setState) {
+      InteractableIrVisualizer? visualizer;
+      Widget? editor;
 
-    if (widget.subViewMode == SubViewMode.split || widget.subViewMode == SubViewMode.visualizer) {
-      visualizer = InteractableIrVisualizer(
-        enableTimeInput: widget.enableTimeInput,
-        robiConfig: widget.robiConfig,
-        totalTime: widget.irReadResult.totalTime,
-        irCalculatorResult: irCalculatorResult,
-        irPathApproximation: irPathApproximation,
-        irReadPainterSettings: irReadPainterSettings,
-        irReadResult: widget.irReadResult,
-      );
-    }
-    if (widget.subViewMode == SubViewMode.split || widget.subViewMode == SubViewMode.editor) {
-      editor = ListView(
-        padding: const EdgeInsets.all(16),
-        children: [
-          IrPathApproximationSettingsWidget(
-            onSettingsChange: (settings) => setState(() => irReadPainterSettings = settings), settings: irReadPainterSettings,
+      if (subViewMode == SubViewMode.split || subViewMode == SubViewMode.visualizer) {
+        visualizer = InteractableIrVisualizer(
+          enableTimeInput: enableTimeInput,
+          robiConfig: robiConfig,
+          totalTime: irReadResult.totalTime,
+          irCalculatorResult: irCalculatorResult,
+          irPathApproximation: IrCalculator.pathApproximation(
+            irCalculatorResult,
+            irReadPainterSettings.irInclusionThreshold,
+            irReadPainterSettings.ramerDouglasPeuckerTolerance,
           ),
-          const SizedBox(height: 16),
-          IrReadingInfoWidget(
-            selectedRobiConfig: widget.robiConfig,
-            irReadResult: widget.irReadResult,
-            irCalculatorResult: irCalculatorResult,
-          ),
-        ],
-      );
-    }
-
-    switch (widget.subViewMode) {
-      case SubViewMode.editor:
-        return editor!;
-      case SubViewMode.visualizer:
-        return visualizer!;
-      case SubViewMode.split:
-        final screenSize = MediaQuery.of(context).size;
-        final isPortrait = screenSize.width < screenSize.height;
-        return ResizableContainer(
-          direction: isPortrait ? Axis.vertical : Axis.horizontal,
-          divider: ResizableDivider(thickness: 3, color: Colors.grey[800]),
+          irReadPainterSettings: irReadPainterSettings,
+          irReadResult: irReadResult,
+        );
+      }
+      if (subViewMode == SubViewMode.split || subViewMode == SubViewMode.editor) {
+        editor = ListView(
+          padding: const EdgeInsets.all(16),
           children: [
-            ResizableChild(child: visualizer!),
-            ResizableChild(child: editor!),
+            IrPathApproximationSettingsWidget(
+              onSettingsChange: (settings) => setState(() => irReadPainterSettings = settings),
+              settings: irReadPainterSettings,
+            ),
+            const SizedBox(height: 16),
+            IrReadingInfoWidget(
+              selectedRobiConfig: robiConfig,
+              irReadResult: irReadResult,
+              irCalculatorResult: irCalculatorResult,
+            ),
           ],
         );
-    }
+      }
+
+      switch (subViewMode) {
+        case SubViewMode.editor:
+          return editor!;
+        case SubViewMode.visualizer:
+          return visualizer!;
+        case SubViewMode.split:
+          final screenSize = MediaQuery.of(context).size;
+          final isPortrait = screenSize.width < screenSize.height;
+          return ResizableContainer(
+            direction: isPortrait ? Axis.vertical : Axis.horizontal,
+            divider: ResizableDivider(thickness: 3, color: Colors.grey[800]),
+            children: [
+              ResizableChild(child: visualizer!),
+              ResizableChild(child: editor!),
+            ],
+          );
+      }
+    });
   }
 }
