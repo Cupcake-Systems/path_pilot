d9305f8a122ef2e31ac753a46dbba8ad8b797f50
Finn Drünert <finndruenert@gmail.com>
2024-11-17 18:58:46 +0100

    improve settings page

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -11,63 +11,50 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
+    final headerStyle = Theme.of(context).textTheme.titleSmall!.copyWith(color: Theme.of(context).colorScheme.onPrimaryContainer);
+
     return Scaffold(
       appBar: AppBar(title: const Text("Preferences")),
       body: ListView(
         children: [
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Visualizer", style: Theme.of(context).textTheme.headlineSmall),
-                  const Divider(),
-                  ListTile(
-                    leading: const Text("Simulation Frame Rate"),
-                    title: Slider(
-                      value: SettingsStorage.visualizerFps.toDouble(),
-                      onChanged: (value) {
-                        setState(() {
-                          SettingsStorage.visualizerFps = value.round();
-                        });
-                      },
-                      min: 1,
-                      max: 60,
-                      divisions: 59,
-                      label: "${SettingsStorage.visualizerFps} FPS",
-                    ),
-                  ),
-                  ListTile(
-                    title: const Text("Show Milliseconds"),
-                    trailing: Switch(
-                      value: SettingsStorage.showMilliseconds,
-                      onChanged: (value) => setState(() => SettingsStorage.showMilliseconds = value),
-                    ),
-                  )
-                ],
-              ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text("Visualizer", style: headerStyle),
+          ),
+          ListTile(
+            title: const Text("Simulation Frame Rate", style: TextStyle(fontSize: 16)),
+            subtitle: const Text("Adjust the frame rate for smoother or more energy-efficient simulations."),
+            trailing: DropdownButton<int>(
+              value: SettingsStorage.visualizerFps,
+              onChanged: (value) => setState(() => SettingsStorage.visualizerFps = value!),
+              items: const [
+                DropdownMenuItem(value: 10, child: Text("10 FPS")),
+                DropdownMenuItem(value: 30, child: Text("30 FPS")),
+                DropdownMenuItem(value: 60, child: Text("60 FPS")),
+              ],
             ),
           ),
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Advanced", style: Theme.of(context).textTheme.headlineSmall),
-                  const Divider(),
-                  ListTile(
-                    title: const Text("Developer Mode"),
-                    trailing: Switch(
-                      value: SettingsStorage.developerMode,
-                      onChanged: (value) => setState(() {
-                        SettingsStorage.developerMode = value;
-                      }),
-                    ),
-                  ),
-                ],
-              ),
+          const Divider(height: 1),
+          ListTile(
+            title: const Text("Show Milliseconds"),
+            subtitle: const Text("Toggle whether to display milliseconds in the timeline."),
+            trailing: Switch(
+              value: SettingsStorage.showMilliseconds,
+              onChanged: (value) => setState(() => SettingsStorage.showMilliseconds = value),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text("Advanced", style: headerStyle),
+          ),
+          ListTile(
+            title: const Text("Developer Mode"),
+            subtitle: const Text("Enable advanced debugging tools and additional features."),
+            trailing: Switch(
+              value: SettingsStorage.developerMode,
+              onChanged: (value) => setState(() {
+                SettingsStorage.developerMode = value;
+              }),
             ),
           ),
         ],
